4fd6cf4a3d99bab2301b6e45a4f69bb6d531c2cf
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2016-02-10 17:55:16 +0800

    term-oss: implement all peripheral traits

--- src/term-oss/src/lib.rs
+++ src/term-oss/src/lib.rs
@@ -9,6 +9,7 @@ mod modulo;
 use ui::Terminal;
 use audio::Buzzer;
 use chip::{Timer, Video, Audio, Input};
+use std::time::Duration;
 
 #[derive(Default)]
 pub struct Peripheral {
@@ -17,8 +18,33 @@ pub struct Peripheral {
 }
 
 impl Timer for Peripheral {
-    fn wait_next_frame(&self) {
+    fn pump(&mut self) -> bool {
+        const NANOS_PER_FRAME: u32 = 1_000_000_000 / 60;
+        self.term.pump_events(Duration::new(0, NANOS_PER_FRAME))
+    }
+}
 
+impl Video for Peripheral {
+    fn clear(&mut self) {
+        self.term.clear();
+    }
+    fn draw(&mut self, x: isize, y: isize, sprite: &[u8]) -> bool {
+        self.term.flip_sprites(x, y, sprite)
+    }
+    fn present(&self) {
+        self.term.paint();
+    }
+}
+
+impl Audio for Peripheral {
+    fn buzz(&self, on: bool) {
+        self.buzzer.buzz(on);
+    }
+}
+
+impl Input for Peripheral {
+    fn keydown(&self, which: usize) -> bool {
+        self.term.keydown(which)
     }
 }
 
